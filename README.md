a20c295a18b26a086907c89f9e4c141604949d02
yuleniwo <xzm2@qq.com>
2024-11-25 09:04:09 +0800

    Add function 'strlcpy'

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # zte printbl
 Similar to ZTE optical modem's "sendcmd 1 DB p &lt;table name>" and "sendcmd 1 DB all" commands
+
+<pre><code>
+Usage: ptbl &lt;table name&gt;
+</code></pre>
+
+### F663N(uClibc 0.9.32, mips)
+![F663N](./images/ptbl_f663n.png)
+
+### arm(soft float)
+![arm](./images/ptbl_arm.png)
--- ptbl.c
+++ ptbl.c
@@ -38,6 +38,20 @@ int OssDebugPrintf(const char* fmt, ...)
 	return 0;
 }
 
+size_t strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t ret = strlen(src);
+
+	if(size)
+	{
+		size_t len = (ret >= size) ? size - 1 : ret;
+		memcpy(dest, src, len);
+		dest[len] = '\0';
+	}
+
+	return ret;
+}
+
 #ifdef __arm__
 // int my_dbDmNeedHidden(const char* tbl_name, const char *col_name)
 // {
